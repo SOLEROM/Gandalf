ddfe5f7f4e4599f006429b79bce4d3ace8d7e72b
vladSolov <{ID}+{username}@users.noreply.github.com>
2026-03-15 11:03:40 +0200

    update tools

---
+++ 10_tools/clones.sh
@@ -0,0 +1,5 @@
+# clone here local tools 
+# git ignore all local folder here except this file
+
+git clone https://github.com/firecrawl/firecrawl
+git clone https://github.com/Dekelelz/let-them-talk
